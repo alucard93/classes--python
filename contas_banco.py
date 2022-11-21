8974dafa4754aca9497db712fcbb28d7fe65d458
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2022-11-21 19:04:11 -0300

    Update contas_banco.py
    adicionado métodos de consultar saldo e depositar e sacar

--- contas_banco.py
+++ contas_banco.py
@@ -7,9 +7,21 @@ class ContaCorrente():
         self.nome  = nome
         self.cpf   = cpf
         self.saldo = 0
+    
+    def consultar_saldo (self):
+        print('Seu saldo atual é de R${:,.2f}'.format(self.saldo)) 
+    
+    def depositar_ou_sacar (self, valor):
+        self.saldo += valor
+        print('Seu saldo atual é de R${:,.2f}'.format(self.saldo)) 
 
 
 #programa
 
 conta_vinicius = ContaCorrente("Marcus Vinicius", 15311742720)
-print(conta_vinicius.nome, conta_vinicius.cpf, conta_vinicius.saldo)
+# print(conta_vinicius.nome, conta_vinicius.cpf, conta_vinicius.saldo)
+print(conta_vinicius.depositar_ou_sacar(100))
+print(conta_vinicius.consultar_saldo())
+# print(conta_vinicius.depositar_ou_sacar(-20))
+
+# print(conta_vinicius.saldo)
