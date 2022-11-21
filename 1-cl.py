70ffb4ed308fe12ae58973ab1daaa3017ac94cc0
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2022-11-21 17:26:50 -0300

    update  mudar_canal()

--- 1-cl.py
+++ 1-cl.py
@@ -4,6 +4,8 @@ os.system('cls')
 #__init__ - quando eu chamar a class Tv automaticamente ele vai chamar a função do metodo
 # sempre que vc estiver dentro de uma classe e quiser referenciar a classe pode usar o 'self'
 #ex: self.cor = 'preta'  pode-se ler que é uma variavel cor da classe Tv // parametro cor da classe Tv
+#metódo é uma função dentro de uma classe
+# () - indica que é um metódo, se n tiver é um atributo
 class Tv:
 
 #self.atributo igual ao valor inicial
@@ -14,8 +16,14 @@ class Tv:
         self.canal = 'Netflix'
         self.volume = 10
 
+    def mudar_canal(self):
+        self.canal = 'Disney+'
+
+
 #realizando a criação de uma instância da classe Tv - Herença//Reaproveitamento de código
 tv_sala = Tv()
 tv_quarto = Tv()
 
 print(tv_sala.cor)
+tv_sala.mudar_canal()
+print(tv_sala.canal)
